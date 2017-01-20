e43bd1c73e63cf8c33f751ddf0ca250b35c64e21
Whateverton <everton.vilela@live.com>
2017-01-20 19:57:07 -0200

    Game manager created Language function implemented

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,79 @@
+﻿using UnityEngine;
+using System.Xml;
+using System.Collections;
+using System.Collections.Generic;
+
+public enum Language
+{
+    Portuguese,
+    English
+}
+
+public class GameManager : MonoBehaviour {
+    public static GameManager instance;
+
+    public string currentLanguage = "Portuguese";
+    XmlDocument currentLanguageDocument;
+
+    public List<TranslatableObject> translatableObjectsList = new List<TranslatableObject>();
+
+    void OnEnable()
+    {
+        if (instance != null)
+        {
+            Destroy(gameObject);
+            return;
+        }
+
+        instance = this;
+
+        LoadLanguageDocument();
+    }
+
+    public void LoadLanguageDocument()
+    {
+        // Carrega o Xml como texto
+        TextAsset documentText = (TextAsset)Resources.Load("Languages/" + currentLanguage);
+
+        // Transforma o texto em Xml
+        currentLanguageDocument = new XmlDocument();
+        currentLanguageDocument.LoadXml(documentText.ToString());
+    }
+
+    public void ApplyLanguage(Language targetLanguage)
+    {
+        if (targetLanguage == Language.English)
+        {
+            currentLanguage = "English";
+        }
+        else if (targetLanguage == Language.Portuguese)
+        {
+            currentLanguage = "Portuguese";
+        }
+
+        LoadLanguageDocument();
+
+        // Atualiza o termo
+        foreach (TranslatableObject currentObject in translatableObjectsList)
+        {
+            currentObject.ApplyTerm();
+        }
+    }
+
+    public string FindTerm(string nodeName)
+    {
+        foreach (XmlNode term in currentLanguageDocument["LanguageData"])
+        {
+            if (term.LocalName == nodeName)
+            {
+                return term.Attributes["text"].InnerText;
+            }
+        }
+
+        Debug.LogError("Nao encontrou o termo" + nodeName);
+
+        return "NULO";
+
+        //return currentLanguageDocument["LanguageData"][nodeName].Attributes["text"].InnerText;
+    }
+}
---
+++ Assets/Scripts/TranslatableObject.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class TranslatableObject : MonoBehaviour
+{
+    public string term;
+    public bool upper = true;
+
+    public void Start()
+    {
+        GameManager.instance.translatableObjectsList.Add(this);
+        ApplyTerm();
+    }
+
+    public void ApplyTerm()
+    {
+        if (upper)
+        {
+            GetComponent<Text>().text = GameManager.instance.FindTerm(term).ToUpper();
+        }
+        else
+        {
+            GetComponent<Text>().text = GameManager.instance.FindTerm(term);
+        }
+    }
+
+    public void OnDisable()
+    {
+        GameManager.instance.translatableObjectsList.Remove(this);
+    }
+}
