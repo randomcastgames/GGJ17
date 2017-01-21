5ef704dd2f2b8eb56972e300a14e3e648c1d70d7
Lucas Saldanha da Gama Pina <Lucas Saldanha da Gama Pina>
2017-01-21 04:13:39 -0200

    Added explosion script

---
+++ Assets/ExplosionForce2D/script/ExplosionForce2D.cs
@@ -0,0 +1,66 @@
+﻿// Developed by Ananda Gupta
+// info@anandagupta.com
+// http://anandagupta.com
+
+using UnityEngine;
+using System.Collections;
+
+public class ExplosionForce2D : MonoBehaviour
+{
+	public float Power;
+	public float Radius;
+	
+		// Use this for initialization
+		void Start ()
+		{
+	
+		}
+	
+		// Update is called once per frame
+		void FixedUpdate ()
+		{
+# if (UNITY_ANDROID || UNITY_IPHONE)
+
+		if (Input.touchCount > 0 && Input.GetTouch (0).phase == TouchPhase.Began) {
+			Vector3 fingerPos = Input.GetTouch(0).position;
+			fingerPos.z = 10;
+            RaycastHit2D hit = Physics2D.Raycast(GetComponent<Rigidbody2D>().transform.position, Camera.main.ScreenToWorldPoint(fingerPos));
+
+            if (hit)
+            {
+                AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, hit.point, Radius);
+            }
+        }
+
+# endif
+
+# if (UNITY_EDITOR || UNITY_WEBPLAYER)
+
+        if (Input.GetButtonDown("Fire1"))
+        {
+            Vector3 mousePos = Input.mousePosition;
+            mousePos.z = 10;
+
+            RaycastHit2D hit = Physics2D.Raycast(GetComponent<Rigidbody2D>().transform.position,Camera.main.ScreenToWorldPoint(mousePos));
+            
+            if (hit){
+                AddExplosionForce(GetComponent<Rigidbody2D>(), Power * 100, hit.point, Radius);
+            }
+        }
+# endif	
+	
+		}
+
+		public static void AddExplosionForce (Rigidbody2D body, float expForce, Vector3 expPosition, float expRadius)
+		{
+				var dir = (body.transform.position - expPosition);
+				float calc = 1 - (dir.magnitude / expRadius);
+				if (calc <= 0) {
+						calc = 0;		
+				}
+
+				body.AddForce (dir.normalized * expForce * calc);
+		}
+
+
+}
